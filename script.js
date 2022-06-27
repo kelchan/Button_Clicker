caacf80ad82700144827d01c26e236f1dd7580af
kelchan <kelvin.chan131@gmail.com>
2022-06-27 10:57:57 -0700

    added like counter

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function login( input ) {
 function hide( input ) {
     input.remove();
 }
+
+function increaseLike( input ) {
+    var num = input.innerText;
+    num = parseInt(num);
+    num++;
+    input.innerText = num + ' Likes';
+    alert( 'Ninja was liked' );
+}
